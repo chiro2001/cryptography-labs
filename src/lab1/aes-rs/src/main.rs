f8b501fcc77c0bcfad122e4887cc03f52bf80248
Chiro Liang <Chiro2001@163.com>
2022-09-24 15:45:12 +0800

    testing loggers

--- src/lab1/aes-rs/src/main.rs
+++ src/lab1/aes-rs/src/main.rs
@@ -4,6 +4,13 @@ use ndarray::Array;
 use futures::executor::block_on;
 use std::io;
 use std::io::{Bytes, Read, Stdin};
+use log::{info, trace, warn};
+
+#[macro_use]
+extern crate log;
+extern crate simplelog;
+
+use simplelog::*;
 
 #[derive(Debug)]
 struct AES {
@@ -16,18 +23,31 @@ impl AES {
     fn new() -> AES {
         return AES { state: Array::<u8, Ix2>::zeros((4, 4)), w: Array::zeros(44), round: 0 };
     }
-    fn encode() {
-
-    }
+    fn encode() {}
 }
 
-async fn run<T: Read>(stream: Bytes<T>) {
+async fn run<T: Read>(stream: Bytes<T>, key: String) {
+    trace!("a trace log");
+    info!("a info long: {}", "abc");
+
+    info!("{}:{}", file!(), line!());
+
+    error!("Bright red error");
+    info!("This only appears in the log file");
+    debug!("This level is currently not enabled for any logger");
+    // warn!("a warning log: {}, retrying", err);
     for b in stream {
         print!("{}", b.unwrap() as char);
     }
 }
 
 fn main() {
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Trace, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
+            // WriteLogger::new(LevelFilter::Info, Config::default(), File::create("my_rust_binary.log").unwrap()),
+        ]
+    ).unwrap();
     println!("AES-rs!");
     let a = Array::<u8, Ix2>::zeros((4, 4));
     println!("a: {}", a);
@@ -49,6 +69,8 @@ fn main() {
     // let iter = File::open("stdin")?.bytes().chain().into_iter();
     // println!("{}", iter);
 
+    let key = String::from("securitysecurity");
+
     // block_on(run(io::stdin().bytes()));
-    block_on(run(File::open("xmake.lua").unwrap().bytes()));
+    block_on(run(File::open("xmake.lua").unwrap().bytes(), key));
 }
