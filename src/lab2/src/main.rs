a15daa047c24ecfe5cf3b873805ade6addb41e7a
Chiro Liang <Chiro2001@163.com>
2022-09-30 15:25:37 +0800

    add config

--- src/lab2/src/main.rs
+++ src/lab2/src/main.rs
@@ -1,5 +1,25 @@
 mod rsa;
 
-fn main() {
-    println!("Hello, RSA!");
+use std::borrow::Borrow;
+use std::error::Error;
+use std::ops::DerefMut;
+use clap::Parser;
+use crate::rsa::rsa::config::CONFIG;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let config = rsa::rsa::config::Config::parse();
+    println!("Run with config: {:#?}", config);
+    CONFIG.write()?.set(config);
+    println!("Run with CONFIG: {:#?}", CONFIG.read().as_ref().unwrap().as_ref());
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::rsa::*;
+    #[test]
+    fn gen_prime() {
+        let prime = rsa::prime_gen::generate();
+        println!("got prime: {:?}", prime);
+    }
 }
