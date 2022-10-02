82c2ae87c20dd8684cf66cf6cb06944a85a6a050
Chiro Liang <Chiro2001@163.com>
2022-10-02 16:59:56 +0800

    fix xmake

--- xmake.lua
+++ xmake.lua
@@ -17,17 +17,20 @@ add_includedirs("$(projectdir)/src/include")
 for _, dir in ipairs(os.dirs(os.projectdir() .. "/src/*")) do
   local name = getpathname(dir)
   if name ~= "include" then
-    target(name)
-      set_kind("binary")
-      add_files("$(projectdir)/src/" .. name .. "/*.c")
-      add_includedirs("$(projectdir)/src/" .. name)
-    target_end()
-    for _, testpath in ipairs(os.files(os.projectdir() .. "/src/" .. name .. "/tests/*")) do
-      local test = getname(testpath)
-      target(name .. "-" .. test)
+    local sourcefiles = os.files(os.projectdir() .. "/src/" .. name .. "/*.c")
+    if #sourcefiles > 0 then
+      target(name)
         set_kind("binary")
-        add_files(testpath)
+        add_files("$(projectdir)/src/" .. name .. "/*.c")
         add_includedirs("$(projectdir)/src/" .. name)
+      target_end()
+      for _, testpath in ipairs(os.files(os.projectdir() .. "/src/" .. name .. "/tests/*")) do
+        local test = getname(testpath)
+        target(name .. "-" .. test)
+          set_kind("binary")
+          add_files(testpath)
+          add_includedirs("$(projectdir)/src/" .. name)
+      end
     end
   end
 end
